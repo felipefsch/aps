cbe14056888f2a735b3ecf73cf9991d8bfdd497b
Felipe Schmidt <fschmidt@rhrk.uni-kl.de>
2016-08-24 14:14:48 +0200

    Signature for LSH function.

---
+++ src/algorithms/LSH.scala
@@ -0,0 +1,15 @@
+package algorithms
+
+import org.apache.spark.rdd.RDD
+import utils.Args
+
+object LSH {
+  
+  /**
+   * Input:
+   * -rdd: input as (RankID, [Rankings]) tuples
+   */
+  def filter(rdd: RDD[(Long, Array[Long])]) : Unit = {
+    
+  }
+}
