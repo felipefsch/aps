b347a31b9b41d8b19c50bb9b7d217e94aac8dd5c
Felipe Schmidt <fschmidt@rhrk.uni-kl.de>
2016-08-26 11:12:45 +0200

    Adding duplicates grouping. Type mismatch for dynamic typing needs to be corrected.

--- src/utils/CartesianProduct.scala
+++ src/utils/CartesianProduct.scala
@@ -17,7 +17,7 @@ object CartesianProduct {
    * -product: cartesian product of RDD elements without self product and no
    * symmetry, i.e., output (1,2) but not (2,1). Lower ID always on left.
    */
-  def orderedWithoutSelf[T] ( in: RDD[(Long, T)] ) : RDD[((Long, T), (Long, T))] = {
+  def orderedWithoutSelf[T] ( in: RDD[(String, T)] ) : RDD[((String, T), (String, T))] = {
     
     val product = in.cartesian(in)
     val productFiltered = product.filter(x => x._1._1 < x._2._1)
