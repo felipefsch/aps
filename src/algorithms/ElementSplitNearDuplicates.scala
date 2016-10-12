a9869f7f5afeaa7559c182bb006e5b0e7c6bceb3
Felipe Schmidt <fschmidt@rhrk.uni-kl.de>
2016-10-12 14:49:49 +0200

    Adding element split with near duplicates

---
+++ src/algorithms/ElementSplitNearDuplicates.scala
@@ -0,0 +1,61 @@
+package algorithms
+
+import utils._
+import org.apache.spark.rdd.EmptyRDD
+
+object ElementSplitNearDuplicates {
+  
+  def main(args: Array[String]): Unit = {
+    Args.parse(args)
+    
+    // Variables not modifiable. Important when running on a cluster
+    // so that all nodes have the correct values
+    val output = Args.output + "ElementSplit_c"
+    val masterIp = Args.masterIp
+    val threshold = Args.threshold
+    val threshold_c = Args.threshold_c
+    val normThreshold = Args.normThreshold
+    val normThreshold_c = Args.normThreshold_c
+    val input = Args.input
+    val k = Args.k
+    val n = Args.n
+    val minOverlap = Args.minOverlap
+    val hdfsUri = Args.hdfsUri
+    val partitions = Args.partitions
+    val COUNT = Args.COUNT
+    val DEBUG = Args.DEBUG
+    val STORERESULTS = Args.STORERESULTS      
+    val GROUPDUPLICATES = Args.GROUPDUPLICATES    
+    
+    val sc = Config.getSparkContext(masterIp)
+
+    try {  
+      // Load also sets ranking size k
+      var ranksArray = Load.loadData(input, sc, partitions, k, n)
+
+      var duplicates : org.apache.spark.rdd.RDD[((String, String), Long)] = sc.emptyRDD      
+      if (GROUPDUPLICATES) {
+        ranksArray = Duplicates.groupDuplicates(ranksArray)
+        duplicates = Duplicates.getDuplicates(ranksArray)        
+      }
+
+      var nearDuplicates = ElementSplit.run(ranksArray, threshold_c, k, minOverlap)                                                                                      
+      var ranksNearDuplicates = NearDuplicates.groupNearDuplicates(nearDuplicates.map(x => x._1), ranksArray)
+      
+      var similarRanks = ElementSplit.run(ranksNearDuplicates, threshold + threshold_c, k, minOverlap)
+      similarRanks = NearDuplicates.filterFalseCandidates(similarRanks, threshold)
+      similarRanks = NearDuplicates.expandNearDuplicates(similarRanks, ranksArray, k, threshold, normThreshold, normThreshold_c)
+      
+      // Add near duplicates to result set
+      similarRanks = similarRanks.union(nearDuplicates)
+            
+      var rddUnion = similarRanks.union(duplicates)
+      similarRanks = Duplicates.expandDuplicates(rddUnion)
+      
+      Store.rdd(output, similarRanks, COUNT, STORERESULTS, hdfsUri)       
+ 
+    } finally {
+      Config.closeSparkContext(sc)
+    }
+  }  
+}
